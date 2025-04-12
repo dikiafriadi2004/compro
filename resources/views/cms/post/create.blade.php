7c3432486114b56a85a8c99765367930456a0f28
Diki Afriadi <diki.afriadi2004@gmail.com>
2025-04-12 23:12:49 +0700

    update

--- resources/views/cms/post/create.blade.php
+++ resources/views/cms/post/create.blade.php
@@ -24,119 +24,178 @@
 @endsection
 
 @section('content')
-    <div class="theme-body common-dash" data-simplebar>
-        <div class="custom-container">
+    <div class="theme-body">
+        <div class="custom-container codexedit-profile">
             <div class="row">
-                <div class="col-xxl-12">
-                    <div class="card project-summarytbl">
+                <div class="col-xl-8 col-md-7 cdx-xl-60">
+                    <div class="card">
                         <div class="card-header">
-                            <h4>Create Post</h4>
-                            <div class="row">
-
-                                <div class="mailreact-right">
-                                        <a href="#" class="btn btn-secondary float-end mb-2">Add Post</a>
-                                    <ul class="mailreact-list">
-                                        <li>
-                                            <div class="input-group">
-                                                <div class="input-group-text cdxappsearch-toggle"><i data-feather="search"></i>
-                                                </div>
-                                                <input class="form-control" type="text" placeholder="Search mail">
-                                            </div>
-                                        </li>
-                                    </ul>
-                                    
-                                </div>
-                            </div>
+                            <h4>contact infomation</h4>
                         </div>
                         <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Order</th>
-                                            <th>Project Name</th>
-                                            <th>Project Cost</th>
-                                            <th>Project Status</th>
-                                            <th>Payment</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Racing Game</td>
-                                            <td>$45,376</td>
-                                            <td>Completed</td>
-                                            <td><span class="badge badge-primary">Done</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Cytrust Dashboard</td>
-                                            <td>$40,258</td>
-                                            <td>In Progress</td>
-                                            <td><span class="badge badge-secondary">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Travel App</td>
-                                            <td>$32,256</td>
-                                            <td>Completed</td>
-                                            <td><span class="badge badge-primary">Done</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td>Reyalestet App</td>
-                                            <td>$25,058</td>
-                                            <td>In Progress</td>
-                                            <td><span class="badge badge-secondary">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td>Agriculture Website</td>
-                                            <td>$36,585</td>
-                                            <td>In Progress</td>
-                                            <td> <span class="badge badge-secondary">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>5</td>
-                                            <td>TRavel Website</td>
-                                            <td>$25,255</td>
-                                            <td>Completed</td>
-                                            <td> <span class="badge badge-primary">Done</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>6</td>
-                                            <td>Reyalestet App</td>
-                                            <td>$25,058</td>
-                                            <td>In Progress</td>
-                                            <td><span class="badge badge-secondary">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>7</td>
-                                            <td>Agriculture Website</td>
-                                            <td>$36,585</td>
-                                            <td>In Progress</td>
-                                            <td> <span class="badge badge-secondary">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>8</td>
-                                            <td>TRavel Website</td>
-                                            <td>$25,255</td>
-                                            <td>Completed</td>
-                                            <td> <span class="badge badge-primary">Done</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>9</td>
-                                            <td>TRavel Website</td>
-                                            <td>$25,255</td>
-                                            <td>Completed</td>
-                                            <td> <span class="badge badge-primary">Done</span></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                            <form>
+                                <div class="form-group">
+                                    <div class="small-group">
+                                        <div>
+                                            <label class="form-label">Mobile number</label>
+                                            <input class="form-control" type="number"
+                                                placeholder="Enter Your Mobile Number">
+                                        </div>
+                                        <div>
+                                            <label class="form-label">Email id</label>
+                                            <input class="form-control" type="email" placeholder="Enter Your  Email">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="small-group">
+                                        <div>
+                                            <label class="form-label">Address</label>
+                                            <input class="form-control" type="text" placeholder="Enter Your  Address">
+                                        </div>
+                                        <div>
+                                            <label class="form-label">Website </label>
+                                            <input class="form-control" type="text" placeholder="website link">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="small-group">
+                                        <div>
+                                            <label class="form-label">company</label>
+                                            <input class="form-control" type="text" placeholder="Enter company name">
+                                        </div>
+                                        <div>
+                                            <label class="form-label">Postal/zip code </label>
+                                            <input class="form-control" type="text" placeholder="zip code">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="small-group">
+                                        <div>
+                                            <label class="form-label">home town</label>
+                                            <input class="form-control" type="text" placeholder="Enter Your town city">
+                                        </div>
+                                        <div>
+                                            <label class="form-label">city</label>
+                                            <input class="form-control" type="text" placeholder="Enter Your city">
+                                        </div>
+                                        <div>
+                                            <label class="form-label">country</label>
+                                            <input class="form-control" type="text"
+                                                placeholder="Enter Your website link">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label">about bio</label>
+                                    <textarea class="form-control" placeholder="enter your bio"></textarea>
+                                </div>
+                                <div class="form-group mb-0">
+                                    <div class="group-btn"><a class="btn btn-success" href="#!">Update</a><a
+                                            class="btn btn-danger ms-2" href="#!">Cancel</a></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-5 cdx-xl-40">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>My Infomation</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="info-group mb-3">
+                                <div class="media align-items-center">
+                                    <div class="userimg-wrap me-2"><img class="img-fluid w-50 rounded-2"
+                                            src="../assets/images/avtar/1.jpg" alt=""></div>
+                                    <div class="media-body">
+                                        <h6>Mark Odem</h6>
+                                        <p class="fs-14 text-light"> Ui/Ux desinger</p>
+                                    </div><span class="badge badge-primary"><a href="#!"><i
+                                                class="fa fa-cloud-upload"></i></a></span>
+                                </div>
+                            </div>
+                            <div class="info-group">
+                                <form>
+                                    <div class="form-group">
+                                        <div class="small-group">
+                                            <div>
+                                                <label class="form-label">First Name</label>
+                                                <input class="form-control" type="text" placeholder="your first name"
+                                                    value="Mark">
+                                            </div>
+                                            <div>
+                                                <label class="form-label">Last Name</label>
+                                                <input class="form-control" type="text" placeholder="your last name"
+                                                    value="Odem">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="small-group">
+                                            <div>
+                                                <label class="form-label">Day</label>
+                                                <select class="form-control hidesearch" name="day[]">
+                                                    <option>01</option>
+                                                    <option>02</option>
+                                                    <option>03</option>
+                                                    <option>04</option>
+                                                    <option>05</option>
+                                                    <option>06</option>
+                                                </select>
+                                            </div>
+                                            <div>
+                                                <label class="form-label">Month </label>
+                                                <select class="form-control hidesearch" name="month[]">
+                                                    <option>Jan</option>
+                                                    <option>Feb</option>
+                                                    <option>March</option>
+                                                    <option>April</option>
+                                                    <option>May</option>
+                                                    <option>Jun</option>
+                                                </select>
+                                            </div>
+                                            <div>
+                                                <label class="form-label">Year</label>
+                                                <select class="form-control hidesearch" name="year[]">
+                                                    <option>1992</option>
+                                                    <option>1993</option>
+                                                    <option>1994</option>
+                                                    <option>1995</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label">Designation</label>
+                                        <input class="form-control" type="text" placeholder="your designation name"
+                                            value="Ui/Ux desinger">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label">Email Address</label>
+                                        <input class="form-control" type="text" placeholder="your email"
+                                            value="mark@example.com">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label">Mobile</label>
+                                        <input class="form-control" type="text" placeholder="your mobile number"
+                                            value="+9588489584">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label">Website</label>
+                                        <input class="form-control" type="text" placeholder="your website"
+                                            value="https://Rohi.com">
+                                    </div>
+                                    <div class="form-group mb-0"><a class="btn btn-primary" href="#!">Submit</a>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
